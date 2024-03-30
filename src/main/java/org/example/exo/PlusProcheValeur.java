3a44ed4b06c02e023de8e3632c2937ccc15f73e8
stephane <stephane.betton@gmail.com>
2024-03-30 21:55:41 +0100

    refacto

--- src/main/java/org/example/exo/PlusProcheValeur.java
+++ src/main/java/org/example/exo/PlusProcheValeur.java
@@ -13,33 +13,48 @@ public class PlusProcheValeur {
         if (isPresentInCollection ) {
             return valeurRecherche;
         }
-        //tri croissant de la collection
-        Collection<Integer> listSorted = tableau.stream().sorted().collect(Collectors.toCollection(TreeSet::new));
-        log.info(listSorted.toString());
-        Optional<Integer> premierPlusGrand = listSorted.stream().filter(valeur -> valeur > valeurRecherche).findFirst();
+        Optional<Integer> premierPlusGrand = getPremierPlusGrand(tableau,valeurRecherche);
 
-        //tri decroissant
-        Collection<Integer> reverseOrder = tableau.stream().sorted(Collections.reverseOrder()).collect(Collectors.toCollection(ArrayList::new));
-        log.info(reverseOrder.toString());
-        Optional<Integer> dernierPlusPetit = reverseOrder.stream().filter(valeur -> valeur < valeurRecherche).findFirst();
+        Optional<Integer> dernierPlusPetit = getDernierPlusPetit(tableau,valeurRecherche);
 
 
         if(premierPlusGrand.isPresent() && dernierPlusPetit.isPresent()) {
-            Integer grand = premierPlusGrand.get();
-            Integer petit = dernierPlusPetit.get();
-
-            //calcul l'ecart
-            Integer ecartGrand = grand - valeurRecherche;
-            Integer ecartPetit = valeurRecherche - petit;
-            log.info(String.valueOf(ecartGrand));
-            log.info(String.valueOf(ecartPetit));
-            if (ecartGrand > ecartPetit) {
-                return petit;
-            }
+            return rechercheLePlusPetitEcart(valeurRecherche, premierPlusGrand.get(), dernierPlusPetit.get());
         } else if (premierPlusGrand.isPresent()) {
             return premierPlusGrand.get();
         }
+        if (dernierPlusPetit.isEmpty()) {
+            throw new PlusProcheValeurException("Error.....Bizarre.....Aucune valeur..");
+        }
         return dernierPlusPetit.get();
 
+    }
+    private Optional<Integer> getDernierPlusPetit(Collection<Integer> tableau,
+                                                  int valeurRecherche){
+        //tri decroissant
+        Collection<Integer> reverseOrder = tableau.stream().sorted(Collections.reverseOrder()).collect(Collectors.toCollection(ArrayList::new));
+        log.info("tri decroissant de la collection {}",reverseOrder);
+        return reverseOrder.stream().filter(valeur -> valeur < valeurRecherche).findFirst();
+    }
+    private Optional<Integer> getPremierPlusGrand(Collection<Integer> tableau,
+                                                  int valeurRecherche){
+        //tri croissant de la collection
+        Collection<Integer> listSorted = tableau.stream().sorted().collect(Collectors.toCollection(TreeSet::new));
+        log.info("tri croissant de la collection {}",listSorted);
+        return listSorted.stream().filter(valeur -> valeur > valeurRecherche).findFirst();
+    }
+    private int rechercheLePlusPetitEcart(int valeurRecherche,
+                                 Integer premierPlusGrand,
+                                 Integer dernierPlusPetit) {
+        //calcul l'ecart
+        Integer ecartGrand = premierPlusGrand - valeurRecherche;
+        Integer ecartPetit = valeurRecherche - dernierPlusPetit;
+        log.info("ecartGrand: {}",ecartGrand);
+        log.info("ecartPetit: {}",ecartPetit);
+        if (ecartGrand > ecartPetit) {
+            return dernierPlusPetit;
+        }else{
+            return premierPlusGrand;
+        }
     }
 }
---
+++ src/main/java/org/example/exo/PlusProcheValeurException.java
@@ -0,0 +1,7 @@
+package org.example.exo;
+
+public class PlusProcheValeurException extends RuntimeException {
+    public PlusProcheValeurException(String message) {
+        super(message);
+    }
+}
